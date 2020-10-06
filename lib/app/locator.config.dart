df9b107541e4dd40b465b208498b8b3fed9363d1
Dane Mackier <dane.mackier@outlook.com>
2020-10-06 06:00:03 +0200

    Updates to work with latest flutter version

---
+++ lib/app/locator.config.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../services/api.dart';
+import '../system/app_database.dart';
+import '../services/counter_service.dart';
+import '../services/media_service.dart';
+import '../services/permissions_service.dart';
+import '../services/posts_service.dart';
+import '../ui/views/posts_example/posts_viewmodel.dart';
+import '../services/shared_preferences_service.dart';
+import '../services/third_party_services_module.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
+
+GetIt $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) {
+  final gh = GetItHelper(get, environment, environmentFilter);
+  final thirdPartyServicesModule = _$ThirdPartyServicesModule();
+  gh.lazySingleton<Api>(() => Api());
+  gh.lazySingleton<AppDatabase>(() => AppDatabase());
+  gh.lazySingleton<CounterService>(() => CounterService());
+  gh.lazySingleton<DialogService>(() => thirdPartyServicesModule.dialogService);
+  gh.lazySingleton<MediaService>(() => MediaService());
+  gh.lazySingleton<NavigationService>(
+      () => thirdPartyServicesModule.navigationService);
+  gh.lazySingleton<PermissionsService>(() => PermissionsService());
+  gh.lazySingleton<PostsService>(() => PostsService());
+  gh.lazySingleton<SharedPreferencesService>(() => SharedPreferencesService());
+
+  // Eager singletons must be registered in the right order
+  gh.singleton<PostsViewModel>(PostsViewModel());
+  return get;
+}
+
+class _$ThirdPartyServicesModule extends ThirdPartyServicesModule {
+  @override
+  DialogService get dialogService => DialogService();
+  @override
+  NavigationService get navigationService => NavigationService();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:my_app/ui/setup_dialog_ui.dart';
 import 'package:my_app/ui/views/dialog_example/dialog_example_view.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-import 'app/router.gr.dart';
+import 'app/router.gr.dart' as auto_router;
 
 void main() {
   setupLocator();
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: DialogExampleView(),
       // initialRoute: Routes.startupViewRoute,
-      onGenerateRoute: Router().onGenerateRoute,
+      onGenerateRoute: auto_router.Router().onGenerateRoute,
       navigatorKey: locator<DialogService>().navigatorKey,
     );
   }
